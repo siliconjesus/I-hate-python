97bf1277a1706d1d0764d4c622b3add1da5f8b5a
Jon Mentzell <jmentzell@redhat.com>
2020-02-18 19:50:23 -0500

    Added comments.

--- odd2.py
+++ odd2.py
@@ -1,7 +1,15 @@
+# first iteration - using one of my favorite operators (modulus) to clean up the mess.
 from datetime import datetime
 
 right_this_minute = datetime.today().minute
 
+# the following line probably violates some sort of python principal regarding simplicity
+# Modulus is essentially the remainder function - the way we learned
+# division in school before we learn about decimals.  
+# EXAMPLE: 5 ÷ 2 = 2r1
+# in python we use % to get this - so 5 % 2 = 1.
+# if the remainder is 1 - its 'true'
+# if its zero - its 'false'
 if (right_this_minute % 2):
     print("this minute seems a little odd.")
 else:
--- odd3.py
+++ odd3.py
@@ -1,5 +1,6 @@
 from datetime import datetime
-
+# Extension of previous - do everything at one time - eliminate the need for the extra variable.
+# again - probably breaks some python simplicity guide.
 if (datetime.today().minute % 2):
     print("this minute seems a little odd.")
 else:
