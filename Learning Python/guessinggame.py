6f69e9d2e3f6c231821b0e2725ba44cb64611dea
Jon Mentzell <siliconjesus@gmail.com>
2020-06-28 13:29:17 -0400

    initial testing of values

--- Learning Python/guessinggame.py
+++ Learning Python/guessinggame.py
@@ -2,3 +2,10 @@ answer = 5
 
 print("Please guess a number between 1 and 10: ")
 guess = int(input())
+
+if guess < answer:
+    print("Too low!")
+elif guess > answer:
+    print("Too high!")
+else:
+    print("C O R R E C T !")
