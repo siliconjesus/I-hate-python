9276a81c986ac67ca2f1d699a0cd34d6b3978942
Jon Mentzell <siliconjesus@gmail.com>
2020-06-28 19:28:08 -0400

    break practice / looping practice

---
+++ Learning Python/searching.py
@@ -0,0 +1,10 @@
+shopping_list = ["milk","pasta","eggs","spam","bread","rice"]
+
+item_to_find = "spam"
+found_at = None
+# I guess None ~= null from C/C++
+for index in range(len(shopping_list)):
+    if shopping_list[index] == item_to_find:
+        found_at = index
+        break
+print(f"Item found at {found_at}")
