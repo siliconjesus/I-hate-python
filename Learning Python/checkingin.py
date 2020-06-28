a33b66e243763e711314b3cbfdcf5e08db3cde53
Jon Mentzell <siliconjesus@gmail.com>
2020-06-28 17:53:05 -0400

    in function for sequences

---
+++ Learning Python/checkingin.py
@@ -0,0 +1,11 @@
+# `in` is used in sequences
+# Character strings are a type of sequence
+
+parrot = "Norwegian Blue"
+
+letter = input("Enter a character: ")
+
+if letter in parrot:
+    print("{} is in {}".format(letter, parrot))
+else:
+    print("I don't need that letter.")
