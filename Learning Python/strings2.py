495404a27792c11effc96fddf85f61d954c2dc13
Jon Mentzell <siliconjesus@gmail.com>
2020-06-28 18:53:48 -0400

    sanitizing user input and splitting numbers

--- Learning Python/strings2.py
+++ Learning Python/strings2.py
@@ -1,11 +1,11 @@
-number = "9,223;372:036 854,775;807"
+number = input("Please enter a series of number, using any separators you like: ")
 separators = ""
 
 for char in number:
     if not char.isnumeric():
         separators = separators + char
 
-print(separators)
+# print(separators)
 
 values = "".join(char if char not in separators else " " for char in number).split()
-print([int(val) for val in values])
+print(sum([int(val) for val in values]))
