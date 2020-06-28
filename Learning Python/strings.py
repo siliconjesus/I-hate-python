ef93b04b5f922e286c6e7cff60684b4a6d827dda
Jon Mentzell <siliconjesus@gmail.com>
2020-06-28 12:53:25 -0400

    udemy coursework

---
+++ Learning Python/strings.py
@@ -0,0 +1,13 @@
+name = "Jon"
+age = 24
+
+print(age)
+
+age_in_words = "Twenty Four Years"
+print(name + f" is {age} years old")
+print(type(age))
+print(f"Pi is aproximately {22 / 7:12.50f}")
+
+pi = 22 / 7
+
+print(f"Pi is aproximately {pi:12.50f}")
