8f42f7967c3eb06b0918f58a1d19cf83bda34bcd
Jon Mentzell <siliconjesus@gmail.com>
2020-06-28 20:10:29 -0400

    fixed broken loop

--- Learning Python/adventure.py
+++ Learning Python/adventure.py
@@ -2,6 +2,6 @@ available_exits = ["north", "south", "east", "west"]
 
 chosen_exit = ""
 while chosen_exit not in available_exits:
-    chose_exit = input("Please choose a direction: ")
+    chosen_exit = input("Please choose a direction: ")
 
 print("Congratulations on escaping")
